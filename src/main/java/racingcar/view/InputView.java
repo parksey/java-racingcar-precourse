ec6daa8ed63a43c4ba29313c0ca45813c557eaf8
parksey <parkseyeon99@naver.com>
2022-12-12 21:42:23 +0900

    feat(InputView): 사용자 입력에 따른 메서드 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,31 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getUserInput() {
+        return Console.readLine();
+    }
+
+    /**
+     * 경주할 자동차 이름 리스트
+     */
+    public void readGameCars() {
+
+    }
+
+    public void checkGameCars(String userInput) {
+
+    }
+
+    /**
+     * 시도할 회수 입력 받는 함수
+     */
+    public void readGameTern() {
+
+    }
+
+    public void checkGameTern(String userInput) {
+
+    }
+}
