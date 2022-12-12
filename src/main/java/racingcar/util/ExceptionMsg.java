e08c9cc6e89e1c9df843397ec839ce7acb8aa5e4
parksey <parkseyeon99@naver.com>
2022-12-12 22:15:39 +0900

    refactor(ExceptionMsg): 예외 출력 메세지에 대한 상수 치환

---
+++ src/main/java/racingcar/util/ExceptionMsg.java
@@ -0,0 +1,19 @@
+package racingcar.util;
+
+public enum ExceptionMsg {
+    NULL("아무 값도 입력하지 않았습니다."),
+    NOT_FORMAT("잘못된 입력 형식입니다."),
+    OVER_SIZE("차량 이름의 길이가 한도를 넘었습니다."),
+    NOT_NUMBER("숫자가 아닙니다."),
+    ZERO("0값은 게임을 시작할 수 없습니다.");
+
+    private final String msg;
+
+    ExceptionMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
--- src/main/java/racingcar/view/InputException.java
+++ src/main/java/racingcar/view/InputException.java
@@ -2,20 +2,21 @@ package racingcar.view;
 
 import racingcar.util.CarCode;
 import racingcar.util.CarStatus;
+import racingcar.util.ExceptionMsg;
 
 import java.util.List;
 
 public class InputException {
     public static void nullException(String userInput) {
         if (userInput == null || userInput.isBlank() || userInput.isEmpty()) {
-            throw new IllegalArgumentException("아무 값도 입력하지 않았습니다.");
+            throw new IllegalArgumentException(ExceptionMsg.NULL.getMsg());
         }
     }
 
     public static void notStartEndWithComma(String userInput) {
         if (userInput.startsWith(CarStatus.COMMA.getStatus())
         || userInput.endsWith(CarStatus.COMMA.getStatus())) {
-            throw new IllegalArgumentException("잘못된 입력 형식입니다.");
+            throw new IllegalArgumentException(ExceptionMsg.NOT_FORMAT.getMsg());
         }
     }
     public static void checkEachCarNameException(List<String> inputList) {
@@ -27,7 +28,7 @@ public class InputException {
 
     public static void overNameSizeException(String userInput) {
         if (userInput.length() > CarCode.NAME_SIZE.getCode()) {
-            throw new IllegalArgumentException("차량 이름의 길이가 한도를 넘었습니다.");
+            throw new IllegalArgumentException(ExceptionMsg.OVER_SIZE.getMsg());
         }
     }
 
@@ -39,13 +40,13 @@ public class InputException {
 
     public static void isDigitException(char input) {
         if (!Character.isDigit(input)) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
+            throw new IllegalArgumentException(ExceptionMsg.NOT_NUMBER.getMsg());
         }
     }
 
     public static void zeroException(String userInput) {
         if (userInput.equals(CarStatus.ZERO.getStatus())) {
-            throw new IllegalArgumentException("0값은 게임을 시작할 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMsg.ZERO.getMsg());
         }
     }
 }
