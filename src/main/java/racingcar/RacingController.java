7d6210618a71a87f1651b73ea612ebb5c33897cd
parksey <parkseyeon99@naver.com>
2022-12-12 22:28:17 +0900

    docs(README.md): 기능 명세서 필요 기능 수정

---
+++ src/main/java/racingcar/RacingController.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        // 자동차 이름 입력
+        // 시도 횟수 입력
+
+    }
+}
