6446cd71a9cb98bd30b8a48a92e682656b61fa9e
parksey <parkseyeon99@naver.com>
2022-12-12 22:11:24 +0900

    feat(OutputView): 출력 클래스 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void printMsg(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * 실행 결과 출력 메서드
+     */
+    public void printEachResult() {
+
+    }
+}
